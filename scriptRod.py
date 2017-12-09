ef23deb9c38a7df2e2b73a0fd475cfe26dc40eac
Luke Johnston <lukejohnston@Lukes-MacBook-Pro-4.local>
2017-12-08 20:06:30 -0800

    with these parameters, things sorta start assembling into circles

--- scriptRod.py
+++ scriptRod.py
@@ -24,6 +24,8 @@ snapshot = hoomd.data.make_snapshot(N=num_particles,
 snapshot.particles.position[:] = positions
 snapshot.particles.typeid[:] = [0 for x in range(-10,10)]
 
+snapshot.particles.moment_inertia[:] = [[0,0,10] for x in range(-10,10)]
+
 # set gaussian random velocity for all proteins
 snapshot.particles.velocity[:] = np.random.normal(0.0,
   np.sqrt(0.8 / 1.0), [snapshot.particles.N, 3]);
@@ -32,36 +34,37 @@ snapshot.particles.velocity[:] = np.random.normal(0.0,
 hoomd.init.read_snapshot(snapshot)
 
 rigid = hoomd.md.constrain.rigid();
+# trapezoid particles, adjusted so R is approximately COM (just eyeballing)
 rigid.set_param('R',
                 types=['A','B','C','D'],
-                positions=[(0,0,0),(1.5,9,0),
-                           (2.5,9,0),(4,0,0)]);
+                positions=[(-2,-3,0),(-0.5,6,0),
+                           (0.5,6,0),(2,-3,0)]);
 rigid.create_bodies()
 
 
 nl = hoomd.md.nlist.cell();
 # use gaussian interaction because it is mathematicaaly simple
-gauss = hoomd.md.pair.gauss(r_cut=10, nlist=nl)
+gauss = hoomd.md.pair.gauss(r_cut=50, nlist=nl)
 # same type particles repel
-gauss.pair_coeff.set('A', 'A',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('B', 'B',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('C', 'C',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('D', 'D',epsilon=-100, sigma=1.0);
+gauss.pair_coeff.set('A', 'A',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('B', 'B',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('C', 'C',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('D', 'D',epsilon=20, sigma=2.0);
 # these need to attract so proteins as0le
-gauss.pair_coeff.set('A', 'D',epsilon=100, sigma=1.0);
-gauss.pair_coeff.set('B', 'C',epsilon=100, sigma=1.0);
+gauss.pair_coeff.set('A', 'D',epsilon=-100, sigma=2.0);
+gauss.pair_coeff.set('B', 'C',epsilon=-100, sigma=2.0);
 # everything else repels
-gauss.pair_coeff.set('A', 'B',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('A', 'C',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('B', 'D',epsilon=-100, sigma=1.0);
-gauss.pair_coeff.set('C', 'D',epsilon=-100, sigma=1.0);
+gauss.pair_coeff.set('A', 'B',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('A', 'C',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('B', 'D',epsilon=20, sigma=2.0);
+gauss.pair_coeff.set('C', 'D',epsilon=20, sigma=2.0);
 
 # center of mass particles don't do anything
-gauss.pair_coeff.set('R', 'R',epsilon=0, sigma=1.0);
-gauss.pair_coeff.set('R', 'A',epsilon=0, sigma=1.0);
-gauss.pair_coeff.set('R', 'B',epsilon=0, sigma=1.0);
-gauss.pair_coeff.set('R', 'C',epsilon=0, sigma=1.0);
-gauss.pair_coeff.set('R', 'D',epsilon=0, sigma=1.0);
+gauss.pair_coeff.set('R', 'R',epsilon=0, sigma=2.0);
+gauss.pair_coeff.set('R', 'A',epsilon=0, sigma=2.0);
+gauss.pair_coeff.set('R', 'B',epsilon=0, sigma=2.0);
+gauss.pair_coeff.set('R', 'C',epsilon=0, sigma=2.0);
+gauss.pair_coeff.set('R', 'D',epsilon=0, sigma=2.0);
 
 
 rigidTest = hoomd.group.rigid_center();
